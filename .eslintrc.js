4d72cdab73706f01980b4d5f57ca20908b2a10b1
Lee <43488305+poiu694@users.noreply.github.com>
2022-06-17 14:48:33 +0900

    chore: lint 충돌 해결
    https://stackoverflow.com/questions/64116378/error-while-loading-rule-typescript-eslint-dot-notation

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: ['tsconfig.json'],
   },
   plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
   rules: {
