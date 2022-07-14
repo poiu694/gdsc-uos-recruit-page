9ee295ebe8d7aa93129a044f2fff3f0f8b889625
Lee <43488305+poiu694@users.noreply.github.com>
2022-07-14 15:44:25 +0900

    feat: Add eslint dependency into hooks

---
+++ packages/hooks/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: ['@gdsc-uos-recruit-page/eslint-config-custom'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+};
