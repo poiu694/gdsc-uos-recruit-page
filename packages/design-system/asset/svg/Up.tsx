ec5e4f9b301dfb6486c62f9e968808a069534566
Lee <43488305+poiu694@users.noreply.github.com>
2022-07-16 00:09:10 +0900

    feat: Add basic icon for repo

---
+++ packages/design-system/asset/svg/Up.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Up = () => {
+  return (
+    <svg
+      width='28'
+      height='24'
+      viewBox='0 0 28 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M20.9864 15L14.2262 9L7.46606 15'
+        stroke='#353535'
+        stroke-width='1.5'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+    </svg>
+  );
+};
+
+export default Up;
---
+++ packages/design-system/asset/svg/index.ts
@@ -0,0 +1,5 @@
+export { default as Up } from './Up';
+export { default as Down } from './Down';
+export { default as Blog } from './Blog';
+export { default as Github } from './Github';
+export { default as Gmail } from './Gmail';
