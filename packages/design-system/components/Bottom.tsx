20b7d265c749b305d22b319522f9baed87e83709
Lee <43488305+poiu694@users.noreply.github.com>
2022-07-21 16:01:35 +0900

    design: Change background color to default value in header, bottom

--- packages/design-system/components/Bottom.tsx
+++ packages/design-system/components/Bottom.tsx
@@ -1,18 +1,18 @@
-import styled from '@emotion/styled';
-import { theme } from '../theme';
-import Icon from './Icon';
-import Typography from './Typography';
+import styled from "@emotion/styled";
+import { theme } from "../theme";
+import Icon from "./Icon";
+import Typography from "./Typography";
 
 function Bottom() {
   return (
     <Wrapper>
-      <Typography type='body5' color={theme.palette.gray300}>
+      <Typography type="body5" color={theme.palette.gray300}>
         © CopyRight GDSC UOS. All Right Reserved.
       </Typography>
       <IconWrapper>
-        <Icon type='github' color={theme.palette.blue400} />
-        <Icon type='blog' color={theme.palette.coreYellow} />
-        <Icon type='gmail' color={theme.palette.red200} />
+        <Icon type="github" color={theme.palette.blue400} />
+        <Icon type="blog" color={theme.palette.coreYellow} />
+        <Icon type="gmail" color={theme.palette.red200} />
       </IconWrapper>
     </Wrapper>
   );
@@ -22,6 +22,8 @@ const Wrapper = styled.div`
   width: 80%;
   margin: 100px auto 16px auto;
 
+  background-color: ${theme.colors.background};
+
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -29,7 +31,7 @@ const Wrapper = styled.div`
 
 const IconWrapper = styled.div`
   display: flex;
-  gap: 16px;
+  gap: ${theme.padding.md}px;
 `;
 
 export default Bottom;
