c8b7cb566669bd48782ba2b5cd90812f7187fdd1
Lee <43488305+poiu694@users.noreply.github.com>
2022-07-22 14:24:23 +0900

    fix: build error

--- service/next.config.js
+++ service/next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
-const withTM = require("next-transpile-modules")(["@gdsc-uos-recruit-page/hooks", "@gdsc-uos-recruit-page/design-system"]);
+const withTM = require('next-transpile-modules')([
+  '@gdsc-uos-recruit-page/hooks',
+  '@gdsc-uos-recruit-page/design-system',
+  '@gdsc-uos-recruit-page/eslint-config-custom',
+  '@gdsc-uos-recruit-page/tsconfig-custom',
+]);
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: true
+  }
 }
 
 module.exports = withTM(nextConfig);
