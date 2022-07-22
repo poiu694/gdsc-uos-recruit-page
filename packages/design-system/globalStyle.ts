ac65eaf23e44a1fda4ac139577a87e5fb8f6019e
Lee <43488305+poiu694@users.noreply.github.com>
2022-07-22 14:42:01 +0900

    feat: Apply NotoSans fonts

--- packages/design-system/globalStyle.ts
+++ packages/design-system/globalStyle.ts
@@ -4,6 +4,10 @@ import emotionReset from 'emotion-reset';
 /* TODOS: add defualt value */
 const globalStyle = css`
   ${emotionReset}
+
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+
+  font-family: 'Noto Sans KR', sans-serif;
 `;
 
 export default globalStyle;
