dcd451230ffc35d8fe02b79bb8519d96c5e1ee6c
Lee <43488305+poiu694@users.noreply.github.com>
2022-08-01 14:05:12 +0900

    refactor: manage team type in design system

--- packages/design-system/@types/Team.ts
+++ packages/design-system/@types/Team.ts
@@ -1,10 +1,13 @@
-export const Team = {
+const Team = {
+  common: 'common',
   frontend: 'frontend',
   backend: 'backend',
   mobile: 'mobile',
   data_ml: 'data_ml',
+  all: 'all',
 } as const;
 
-
 type TeamType = typeof Team;
+export type TeamKeyType = keyof TeamType;
 export type TeamValueType = TeamType[keyof TeamType];
+export type TeamContent<T> = { [k in TeamKeyType]: T };
