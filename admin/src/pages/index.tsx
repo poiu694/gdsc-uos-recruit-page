7240740be7f0619f22d9f6dce4a76fe73fb0b4b9
Lee <43488305+poiu694@users.noreply.github.com>
2022-08-08 01:05:14 +0900

    test: test ci

--- admin/src/pages/index.tsx
+++ admin/src/pages/index.tsx
@@ -1,4 +1,5 @@
 export { default as MainPage } from './MainPage';
 export { default as LoginPage } from './LoginPage';
 export { default as GoogleFormApplicationListPage } from './GoogleFormApplicationListPage';
+
 export { default as GoogleFormApplicationDetailPage } from './GoogleFormApplicationDetailPage';
