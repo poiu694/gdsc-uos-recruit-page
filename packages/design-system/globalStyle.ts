2a3f438667998fed12babe59e728e097a98a100e
Lee <43488305+poiu694@users.noreply.github.com>
2022-11-30 15:29:11 +0900

    refactor: 필요한 폰트만 로드

--- packages/design-system/globalStyle.ts
+++ packages/design-system/globalStyle.ts
@@ -5,9 +5,35 @@ import { theme } from './theme';
 const globalStyle = css`
   ${emotionReset}
 
-  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+  @font-face {
+    font-family: 'Noto Sans KR';
+    font-style: normal;
+    font-weight: 400;
+    src: url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular.woff2)
+        format('woff2'),
+      url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular.woff)
+        format('woff'),
+      url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular.otf)
+        format('opentype');
+  }
 
-  background-color: ${theme.colors.background};
+  @font-face {
+    font-family: 'Noto Sans KR';
+    font-style: normal;
+    font-weight: 700;
+    src: url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Bold.woff2)
+        format('woff2'),
+      url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Bold.woff)
+        format('woff'),
+      url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Bold.otf)
+        format('opentype');
+  }
+
+  body {
+    font-family: 'Noto Sans KR', sans-serif;
+    background-color: ${theme.colors.background};
+    font-display: swap;
+  }
 `;
 
 export default globalStyle;
