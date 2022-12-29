539c871139dde28f2bcb1916c83e549ab77414bb
Lee <43488305+poiu694@users.noreply.github.com>
2022-12-29 10:51:29 +0900

    feat: LoginPage 제작

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,7 +1,14 @@
+import { Global, ThemeProvider } from '@emotion/react';
+import { globalStyle, theme } from '@gdsc-uos-recruit-page/design-system';
 import MainRoute from './routes';
 
 function App() {
-  return <MainRoute />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyle} />
+      <MainRoute />
+    </ThemeProvider>
+  );
 }
 
 export default App;
