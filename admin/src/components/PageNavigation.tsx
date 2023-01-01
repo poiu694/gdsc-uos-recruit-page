a6d91444e96f75ae8b3cc5923ad467a0de9b4c16
Lee <43488305+poiu694@users.noreply.github.com>
2023-01-01 23:52:12 +0900

    feat: PageNavigation 제작

---
+++ admin/src/components/PageNavigation.tsx
@@ -0,0 +1,95 @@
+import styled from '@emotion/styled';
+import {
+  Button,
+  ClickableIcon,
+  theme,
+  Typography,
+} from '@gdsc-uos-recruit-page/design-system';
+import { ButtonHierarchy } from '@gdsc-uos-recruit-page/design-system/components/Button';
+import { PageOptions } from '../hooks/usePagination';
+
+export interface Props {
+  pageOptions: PageOptions;
+  handleChangePage: (page: number) => void;
+}
+
+function PageNavigation({
+  pageOptions,
+  handleChangePage,
+  ...restProps
+}: Props) {
+  const { startPage, endPage, currentPage, totalPages } = pageOptions;
+  const prevPage = currentPage - 1;
+  const nextPage = currentPage + 1;
+  const pages = [...Array(endPage - startPage + 1).keys()].map(
+    (_, idx) => startPage + idx
+  );
+
+  return (
+    <Wrapper {...restProps}>
+      <ClickableIcon
+        iconProps={{ type: 'doubleLeft' }}
+        onClick={() => handleChangePage(1)}
+        disabled={currentPage === 1}
+      />
+      <ClickableIcon
+        iconProps={{ type: 'left' }}
+        onClick={() => handleChangePage(prevPage)}
+        disabled={currentPage === 1}
+      />
+      <NavigationList>
+        {pages.map((page) => {
+          const isCurrentPage = page === currentPage;
+          return (
+            <li key={String(page)}>
+              <NavigationItemButton
+                borderRadius={4}
+                disabled={isCurrentPage}
+                hierarchy={ButtonHierarchy.Parent}
+                onClick={() => handleChangePage(page)}
+              >
+                <Typography
+                  type="body3"
+                  color={
+                    isCurrentPage
+                      ? theme.colors.primary.white
+                      : theme.palette.gray400
+                  }
+                >
+                  {page}
+                </Typography>
+              </NavigationItemButton>
+            </li>
+          );
+        })}
+      </NavigationList>
+      <ClickableIcon
+        iconProps={{ type: 'right' }}
+        onClick={() => handleChangePage(nextPage)}
+        disabled={currentPage === totalPages}
+      />
+      <ClickableIcon
+        iconProps={{ type: 'doubleRight' }}
+        onClick={() => handleChangePage(totalPages)}
+        disabled={currentPage === totalPages}
+      />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NavigationList = styled.ol`
+  display: flex;
+  gap: 16px;
+`;
+
+const NavigationItemButton = styled(Button)`
+  padding: 8px;
+`;
+
+export default PageNavigation;
