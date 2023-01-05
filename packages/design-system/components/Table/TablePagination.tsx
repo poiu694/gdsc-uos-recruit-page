4c01d56dc353fd133d78f85008a988eecb47d879
Lee <43488305+poiu694@users.noreply.github.com>
2023-01-05 21:56:15 +0900

    feat: Table 내 page마다 보일 row갯수 옵션 조절하는 컴포넌트 추가

---
+++ packages/design-system/components/Table/TablePagination.tsx
@@ -0,0 +1,62 @@
+import styled from '@emotion/styled';
+import { theme } from '../../theme';
+import Typography from '../Typography';
+
+interface Props {
+  page: number;
+  totalCount: number;
+  pageSize: number;
+  pageSizeOptions: number[];
+  onPageSizeOptionsChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+}
+
+function TablePagination({
+  page,
+  pageSize,
+  totalCount,
+  pageSizeOptions,
+  onPageSizeOptionsChange,
+}: Props) {
+  return (
+    <Wrapper>
+      <Typography type="body4" textAlign="end">
+        Rows per page:
+      </Typography>
+      <PageSizeSelect value={pageSize} onChange={onPageSizeOptionsChange}>
+        {pageSizeOptions.map((candidatePageSize) => (
+          <option
+            value={candidatePageSize}
+            key={candidatePageSize}
+            defaultChecked={pageSize === candidatePageSize}
+          >
+            {candidatePageSize}
+          </option>
+        ))}
+      </PageSizeSelect>
+      <Typography type="body4" textAlign="end">
+        {(page - 1) * pageSize + 1}-{page * pageSize} of {totalCount}
+      </Typography>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  width: 100%;
+  margin-block: 8px;
+  display: flex;
+  justify-content: end;
+  gap: 16px;
+`;
+
+const PageSizeSelect = styled.select`
+  border: none;
+  outline: none;
+  cursor: pointer;
+  transition: all 0.1s ease-in;
+
+  &:hover {
+    background-color: ${theme.colors.ui.hover};
+  }
+`;
+
+export default TablePagination;
--- packages/design-system/components/Table/index.ts
+++ packages/design-system/components/Table/index.ts
@@ -3,3 +3,4 @@ export { default as Td } from './Td';
 export { default as Table } from './Table';
 export { default as TBody } from './TBody';
 export { default as THead } from './THead';
+export { default as TablePagination } from './TablePagination';
