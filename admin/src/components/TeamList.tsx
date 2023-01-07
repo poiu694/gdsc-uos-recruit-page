e68c896c067258968f4aa9689453d753bcabf283
Lee <43488305+poiu694@users.noreply.github.com>
2023-01-07 21:43:48 +0900

    feat: TeamList 컴포넌트 추가

---
+++ admin/src/components/TeamList.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { theme, Typography } from '@gdsc-uos-recruit-page/design-system';
+
+import {
+  Team,
+  TeamKeyType,
+} from '@gdsc-uos-recruit-page/design-system/@types/Team';
+
+interface Props {
+  activeTeam: TeamKeyType;
+  onClickTeamName: (teamName: TeamKeyType) => void;
+}
+
+function TeamList({ activeTeam, onClickTeamName }: Props) {
+  return (
+    <Wrapper>
+      <TeamListWrapper>
+        <Typography type="h5">팀 리스트</Typography>
+        {Object.values(Team).map((team) => (
+          <ListItem key={team} onClick={() => onClickTeamName(team)}>
+            <Typography
+              type={activeTeam === team ? 'h6' : 'body3'}
+              color={
+                activeTeam === team
+                  ? theme.colors.primary.blue
+                  : theme.palette.gray350
+              }
+            >
+              {team}
+            </Typography>
+          </ListItem>
+        ))}
+      </TeamListWrapper>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div``;
+
+const TeamListWrapper = styled.ul`
+  width: 15rem;
+  height: 100%;
+`;
+
+const ListItem = styled.li`
+  height: 32px;
+  padding: ${theme.padding.md}px ${theme.padding.xlg}px ${theme.padding.md}px
+    ${theme.padding.md}px;
+  cursor: pointer;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  transition: all 0.1s ease-in-out;
+
+  &:hover {
+    background-color: ${theme.colors.ui.hover};
+  }
+`;
+
+export default TeamList;
--- admin/src/components/index.ts
+++ admin/src/components/index.ts
@@ -3,6 +3,7 @@ export { default as Logo } from './Logo';
 export { default as IconText } from './IconText';
 export { default as SideMenu } from './SideMenu';
 export { default as MemoTable } from './MemoTable';
+export { default as TeamList } from './TeamList';
 export { default as SummaryCard } from './SummaryCard';
 export { default as PageNavigation } from './PageNavigation';
 export { default as EvaluationTable } from './EvaluationTable';
