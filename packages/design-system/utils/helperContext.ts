686b0d1f9a9d7a168bca6368b652ab2d24c0bf0b
Lee <43488305+poiu694@users.noreply.github.com>
2023-01-07 20:48:40 +0900

    feat: context type-safe 함수 생성

---
+++ packages/design-system/utils/helperContext.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function createGenericContext<T>() {
+  return React.createContext<T>({} as T);
+}
+
+export function createGenericUseContext<T>(
+  context: React.Context<T>
+): () => NonNullable<T> {
+  return (): NonNullable<T> => {
+    const value: T = React.useContext<T>(context as React.Context<T>);
+
+    return value as NonNullable<T>;
+  };
+}
