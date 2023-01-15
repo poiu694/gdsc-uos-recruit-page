237c9ca820e9b6259786ae9ff642eab95157819d
Lee <43488305+poiu694@users.noreply.github.com>
2023-01-15 14:33:00 +0900

    feat: PropsWithChildren에서 children 강제 및 제네릭 옵셔널하게

--- packages/design-system/utils/helperTypes.ts
+++ packages/design-system/utils/helperTypes.ts
@@ -1,4 +1,13 @@
+import { ReactNode } from 'react';
+
 export type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
 export type XOR<T, U> = T | U extends Record<string, unknown>
   ? (Without<T, U> & U) | (Without<U, T> & T)
   : T | U;
+
+type RequiredChildren = {
+  children: ReactNode;
+}
+export type StrictPropsWithChildren<T extends unknown | void = void> = T extends void ? RequiredChildren : T & {
+  children: ReactNode;
+}
