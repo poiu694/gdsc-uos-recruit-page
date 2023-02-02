9278713d1ec5e1117f799c686bca6afc50c00b4f
Lee <43488305+poiu694@users.noreply.github.com>
2023-02-02 17:39:36 +0900

    refactor: 메인페이지 관리는 깃헙에서 크롤링

--- admin/src/components/ManageRecruitInfoBox/MainRecruitInfoBox.tsx
+++ admin/src/components/ManageRecruitInfoBox/MainRecruitInfoBox.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
-import { Typography } from 'gdsc-uos-design-system';
+import { theme, Typography } from 'gdsc-uos-design-system';
+import { Link } from 'react-router-dom';
 
 import { Flex } from '../styled';
 
@@ -8,28 +9,22 @@ function MainRecruitInfoBox() {
     <Wrapper>
       <Typography type="h6">Main Page 관리</Typography>
       <Flex flexDirection="column" gap={12} style={{ marginTop: 16 }}>
+        <Typography type="body4">메인 페이지는 Github에서 관리됩니다.</Typography>
         <Typography type="body4">
-          소개글: 2022년부터 함께 할 사람을 찾습니다.
-        </Typography>
-        <Typography type="body4">
-          채용글: 저희와 함께 할 새로운 멤버를 구합니다.\n많은 지원
-          부탁드립니다.
-        </Typography>
-        <Typography type="body4">서류 접수: 2022.08.15 ~ 2022.08.29</Typography>
-        <Typography type="body4">서류 발표: 2022.09.01</Typography>
-        <Typography type="body4">
-          인터뷰 진행: 2022.09.03 ~ 2022.09.09
-        </Typography>
-        <Typography type="body4">최종 발표: 2022.09.14</Typography>
-        <Typography type="body4">
-          팀 소개글: 성장을 원하고 열정적인 사람을 찾습니다.\n당신의 열정을
-          뽐내주세요.
+          <a href="https://github.com/GDSC-University-of-Seoul" target="_blank" rel="external">
+            관리하러가기
+          </a>
         </Typography>
       </Flex>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.section``;
+const Wrapper = styled.section`
+  & div > a {
+    text-decoration: none;
+    color: ${theme.colors.primary.blue};
+  }
+`;
 
 export default MainRecruitInfoBox;
