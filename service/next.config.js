538ebe7f07c197514a1cca71a48d10004e1b2a7f
Lee <43488305+poiu694@users.noreply.github.com>
2023-02-23 15:04:08 +0900

    fix: gdsc-hooks 삭제

--- service/next.config.js
+++ service/next.config.js
@@ -2,7 +2,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   transpilePackages: [
-    'gdsc-uos-hooks',
     'gdsc-uos-design-system',
     'gdsc-uos-eslint-config-custom',
     'gdsc-uos-tsconfig-custom',
