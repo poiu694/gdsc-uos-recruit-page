7a98f20ba37ef2705a0f1612662e44a7062f5c71
Lee <43488305+poiu694@users.noreply.github.com>
2023-02-27 23:15:23 +0900

    chore: clip-path => clipPath

--- packages/design-system/src/asset/svg/Sun.tsx
+++ packages/design-system/src/asset/svg/Sun.tsx
@@ -10,7 +10,7 @@ const Sun = ({ ...restProps }) => {
       xmlns="http://www.w3.org/2000/svg"
       {...restProps}
     >
-      <g clip-path="url(#clip0_102_1527)">
+      <g clipPath="url(#clip0_102_1527)">
         <path
           d="M12 17C14.7614 17 17 14.7614 17 12C17 9.23858 14.7614 7 12 7C9.23858 7 7 9.23858 7 12C7 14.7614 9.23858 17 12 17Z"
           stroke={theme.palette.gray300}
