d817402754b2ef4f5a6a62052a4baeb70e1fbbcb
Lee <43488305+poiu694@users.noreply.github.com>
2023-03-06 01:02:59 +0900

    feat: testMocking 서버 연결

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,16 +1,35 @@
+import React from 'react';
 import { Global, ThemeProvider } from '@emotion/react';
 import { globalStyle, theme } from 'gdsc-uos-design-system';
-import React from 'react';
 
-import { context as APIContext, TestAPI } from './api';
 import MainRoute from './routes';
+import { context as APIContext, TestAPI } from './api';
 
 const apiInstance = {
   test: new TestAPI(),
 } as const;
 
 function App() {
+  const [isReadyRender, setIsReadyRender] = React.useState(
+    import.meta.env.VITE_API_MOCKING_ON !== 'enable',
+  );
   const api = React.useMemo(() => apiInstance, []);
+
+  React.useLayoutEffect(() => {
+    async function runMockServer() {
+      const { initMocks } = await import('gdsc-uos-api');
+      await initMocks();
+      setIsReadyRender(true);
+    }
+    const isMockingServerOn = import.meta.env.VITE_API_MOCKING_ON === 'enable';
+    if (isMockingServerOn && !isReadyRender) {
+      runMockServer();
+    }
+  }, []);
+
+  if (!isReadyRender) {
+    return null;
+  }
 
   return (
     <APIContext.Provider value={api}>
