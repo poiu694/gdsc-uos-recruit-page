bc1b7fde3a3a2cd9992bee7a7d50d7a4e53c1f27
Lee <43488305+poiu694@users.noreply.github.com>
2023-03-20 15:49:13 +0900

    fix: app 이름 겹침으로 인한 apps로 변경

--- apps/service/next.config.js
+++ apps/service/next.config.js
@@ -1,11 +1,13 @@
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  transpilePackages: [
-    '@gdsc-uos/ui',
-    '@gdsc-uos/eslint',
-    '@gdsc-uos/tsconfig',
-  ],
+  transpilePackages: ['@gdsc-uos/ui', '@gdsc-uos/eslint', '@gdsc-uos/tsconfig'],
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
   webpack: (config) => {
     config.watchOptions = {
       poll: 1000,
